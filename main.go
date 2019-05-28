2ff33db379afa1139ab61a040d3b00da5e14cbc0
woodenstick <309136832@qq.com>
2019-05-28 11:46:22 +0800

    post表单参数接收和json返回

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"net/http"
-	"io"
 )
 
 func main() {
@@ -11,7 +10,27 @@ func main() {
 		// 数据库操作
 		// 逻辑处理
 		// restapi json/xml 返回
-		io.WriteString(writer,"HelloWorld!")
+		// 获取前段传递的post表单参数
+		// 普通的post表单请求，Content-Type=application/x-www-form-urlencoded
+		// 解析参数
+		request.ParseForm()
+		mobile := request.PostForm.Get("mobile")
+		passwd := request.PostForm.Get("passwd")
+
+		loginok := false
+		if(mobile == "15812341234" && passwd == "000000"){
+			loginok = true
+		}
+		str := `{"code":0,"data":{"id":1,"token":"test"}}`
+		if(!loginok){
+			str = `{"code":-1,"msg":"账号或密码错误"}`
+		}
+		// header的Content-Type为application/json
+		writer.Header().Set("Content-Type","application/json")
+		// 设置200状态
+		writer.WriteHeader(http.StatusOK)
+		// 输出
+		writer.Write([]byte(str))
 	})
 	// 启动web服务器
 	http.ListenAndServe(":8080",nil)
